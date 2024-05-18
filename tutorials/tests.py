0cf5efd7b2013616a6c0aae4e8d33b937806f7ff
muyideeno <m_odenike@yahoo.com>
2024-05-18 00:11:12 -0500

    Fixed fixture

--- tutorials/tests.py
+++ tutorials/tests.py
@@ -6,19 +6,30 @@ from tutorials.models import Tutorial
 # Create your tests here.
 
 def test_homepage_access():
-         url = reverse('home')     
-         assert url == "/"
+    url = reverse('home')     
+    assert url == "/"
 
 
-# @pytest.mark.django_db
-# def test_create_tutorial(tutorial):
-#     tutorial = Tutorial.objects.create(
-#         title='Pytest',
-#         tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
-#         description='Tutorial on how to apply pytest to a Django application',
-#         published=True
-#     )
-#     assert tutorial.title == "Pytest"
+@pytest.fixture
+def tutorial(db):
+    tutorial = Tutorial.objects.create(
+        title='Pytest',
+        tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
+        description='Tutorial on how to apply pytest to a Django application',
+        published=True
+    )
+    return tutorial
+
+
+@pytest.mark.django_db
+def test_create_tutorial(tutorial):
+    tutorial = Tutorial.objects.create(
+        title='Pytest',
+        tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
+        description='Tutorial on how to apply pytest to a Django application',
+        published=True
+    )
+    assert tutorial.title == "Pytest"
 
 
 
